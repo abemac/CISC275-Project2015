e331da6df4d449c39ef8f0d1323dace309301037
Arjun <patelaj94@yahoo.com>
2015-10-31 12:33:45 -0400

    new stuff - Arjun

--- src/stubs/Net.java
+++ src/stubs/Net.java
@@ -2,6 +2,8 @@ package stubs;
 
 public class Net extends Enemy {
 
+	// Two types of nets will be used to capture the fish in OverfishingGame
+	// Fish will avoid both - Big net can capture more fish in one try
 	public static int BIGNET = 0;
 	public static int LILNET = 1;
 	
@@ -10,6 +12,7 @@ public class Net extends Enemy {
 		this.type = type;
 	}
 	
+	// Movement of the fishnet will be random as fish try to avoid them
 	@Override
 	public void act() {
 		moveRandomly();
