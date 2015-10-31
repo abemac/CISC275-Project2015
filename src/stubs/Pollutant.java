145d218b4270a685e0da4d5f266a5e3039b21920
Arjun <patelaj94@yahoo.com>
2015-10-31 12:38:35 -0400

    new stuff - Arjun

--- src/stubs/Pollutant.java
+++ src/stubs/Pollutant.java
@@ -2,15 +2,17 @@ package stubs;
 
 public class Pollutant extends Enemy {
 
+	// Forms of Pollutants
 	public static final int OIL = 0;
 	public static final int SEWAGE = 1;
 	public static final int FERTILIZER = 2;
 
-	private int type;
-	private boolean isRemoved;
+	private int type;	// Will be used to launch pollutant into game
+	private boolean isRemoved; // Make true if user has cleaned up the pollutatn
 	private int nastiness;
-	private double sourcePos;
+	private double sourcePos; // Position of the pollutant
 	
+	// call to decide type of pollutant
 	public Pollutant(int type){
 		this.type = type;
 	}
