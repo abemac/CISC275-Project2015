0da9d6040b5092586ee7d12a45135292b7f42cd8
Sunny Antala <santala@udel.edu>
2015-10-31 11:58:56 -0400

    works?

--- src/stubs/Enemy.java
+++ src/stubs/Enemy.java
@@ -2,30 +2,34 @@ package stubs;
 
 public abstract class Enemy {
 
-	private double xPos,yPos;
-	private int health;
+	private double xPos,yPos;	//Enemy has location
+	private int health;			//Enemy has health
 	
 	public Enemy(){
-		
+		//create enemies with x and y position and health level
 	}
 	
 	
 	
-	public abstract void act();
+	public abstract void act(); //enemies will do something to attack Character
 	
 	public void onTick(){
+		//this will update act 60 times/sec
 		act();
 	}
 	
 	
 	public double getX(){
+		//returns X position of Enemy
 		return xPos;
 	}
 	public double getY(){
+		//returns Y position of Enemy
 		return yPos;
 	}
 	
 	public int getHealth(){
+		//return health of Enemy
 		return health;
 	}
 	
