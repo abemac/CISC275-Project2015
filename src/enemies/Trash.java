fdeb06694be6a2d23810398daa12e6749aaff681
Abraham McIlvaine <abemac@udel.edu>
2015-11-01 20:54:34 -0500

    Update Trash.java

--- src/enemies/Trash.java
+++ src/enemies/Trash.java
@@ -3,11 +3,11 @@ package enemies;
 public class Trash extends Enemy {
 	// Different Trash that will need to be picked up to clean Estuary 
 	public static final int BANANA =0;
-	public static final int TIRE =0;
-	public static final int SODA_CAN =0;
-	public static final int BOOK =0;
-	public static final int GARBAGE_BAG =0;
-	public static final int SHOPPING_CART =0;
+	public static final int TIRE =1;
+	public static final int SODA_CAN =2;
+	public static final int BOOK =3;
+	public static final int GARBAGE_BAG =4;
+	public static final int SHOPPING_CART =5;
 	
 	
 	private int type;
