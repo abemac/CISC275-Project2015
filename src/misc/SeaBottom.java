ee303b5380d5ca3f6841274410560a73d23c6464
Abraham McIlvaine <abemac@udel.edu>
2015-11-10 11:46:38 -0500

    new stuff

---
+++ src/misc/SeaBottom.java
@@ -0,0 +1,34 @@
+package misc;
+
+import java.awt.Graphics2D;
+
+public class SeaBottom implements Renderable,Tickable {
+
+	
+	
+	private int topY,botY;
+	
+	
+	public SeaBottom(int topY,int botY){
+		this.topY = topY;
+		this.botY = botY;
+
+		
+	}
+	
+	
+	@Override
+	public void onTick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		
+		
+	}
+	
+	
+
+}
