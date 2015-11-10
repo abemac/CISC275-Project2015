314bc6aa3f4a51b59adf1724c9f62a194a5065e7
Abraham McIlvaine <abemac@udel.edu>
2015-11-10 16:21:10 -0500

    new stuff

--- src/view/EstuaryView2.java
+++ src/view/EstuaryView2.java
@@ -1,9 +1,11 @@
 package view;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import games.EstuaryAdventureMain;
 import misc.Renderable;
 
 public class EstuaryView2 extends JPanel {
@@ -13,15 +15,25 @@ public class EstuaryView2 extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private Renderable current;
 	
-	
-	
-	public void paint(Graphics g){
+	public EstuaryView2(){
+		setDoubleBuffered(true);
 		
 	}
 	
-	public void render(Renderable r){
+	
+	public void paint(Graphics g){
+		//Graphics2D g2d = (Graphics2D)g;
+		current.render((Graphics2D)g);
+		//g.dispose();
 		
+		
+		
+	}
+	
+	public void setRenderable(Renderable r){
+		this.current = r;
 	}
 	
 	
