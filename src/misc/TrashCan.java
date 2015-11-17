7b2f97334ed6aa5de747f0d79768c2de7155cb8d
Abraham McIlvaine <abemac@udel.edu>
2015-11-17 14:32:08 -0500

    new stuff abraham

--- src/misc/TrashCan.java
+++ src/misc/TrashCan.java
@@ -1,21 +1,34 @@
 package misc;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class TrashCan implements Tickable,Renderable{
 
 	
 	private double xPos,yPos;
+	private BufferedImage trashCan;
 
 	public TrashCan (double xPos,double yPos){
 		this.xPos=xPos;
 		this.yPos=yPos;
+		loadRes();
+	}
+	
+	
+	private void loadRes(){
+		try {
+			Util.loadImage("/RubbishBin.png", this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
 	@Override
 	public void render(Graphics2D g) {
-		
+		g.drawImage(trashCan, (int)xPos, (int)yPos, null);
 		
 	}
 
