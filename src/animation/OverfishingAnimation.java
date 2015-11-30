f75fec59fcc0bc2e280f468300198aaf09097af6
Abraham McIlvaine <abemac@udel.edu>
2015-11-30 09:28:29 -0500

    New stuff

---
+++ src/animation/OverfishingAnimation.java
@@ -0,0 +1,34 @@
+package animation;
+
+import java.util.ArrayList;
+
+import misc.SoundDoer;
+
+public class OverfishingAnimation {
+
+	
+	private ArrayList<Slide> slides;
+	private SoundDoer soundDoer = new SoundDoer();
+	
+	public OverfishingAnimation(){
+		init();
+		loadRes();
+	}
+	
+	
+	private void loadRes(){
+		
+	}
+	private void init(){
+		slides=new ArrayList<Slide>();
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/animation/Slide.java
@@ -0,0 +1,30 @@
+package animation;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class Slide {
+
+	
+	private BufferedImage image;
+	private int time;
+	private int startTime=0;
+	
+	
+	public Slide(BufferedImage image,int time){
+		this.image=image;
+		this.time=time;
+	}
+	
+	
+	
+	
+	public boolean display(Graphics2D g){
+		
+		
+		return false;
+	}
+	
+	
+	
+}
