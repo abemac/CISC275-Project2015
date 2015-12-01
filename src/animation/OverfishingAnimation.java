e6d52536af08ba25ca41d066b97528c5e833d375
Abraham McIlvaine <abemac@udel.edu>
2015-11-30 22:13:41 -0500

    new stuff abraham

--- src/animation/OverfishingAnimation.java
+++ src/animation/OverfishingAnimation.java
@@ -1,8 +1,11 @@
 package animation;
 
+import java.awt.Color;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import misc.SoundDoer;
+import misc.Util;
 
 public class OverfishingAnimation {
 
@@ -11,16 +14,20 @@ public class OverfishingAnimation {
 	private SoundDoer soundDoer = new SoundDoer();
 	
 	public OverfishingAnimation(){
-		init();
-		loadRes();
+		try {
+			init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
-	private void loadRes(){
-		
-	}
-	private void init(){
+	private void init() throws IOException{
 		slides=new ArrayList<Slide>();
+		slides.add(new Slide(Color.BLACK, 4));
+		slides.add(new Slide(Util.loadImage("/Game1AnimationSlide3.png",
+				Util.getCANVAS_WIDTH_SCALED(),Util.getCANVAS_HEIGHT_SCALED(), this), 4));
+		
 		
 	}
 	
