91c119544dfa6d63093c377905a8cae3207ec61d
Abraham McIlvaine <abemac@udel.edu>
2015-12-02 00:15:20 -0500

    New stuff

--- src/tests/CharacterTests.java
+++ src/tests/CharacterTests.java
@@ -9,6 +9,7 @@ import enemies.TheHuman;
 import enemies.Trash;
 import games.OverfishingGame;
 import junit.framework.TestCase;
+import misc.Vector;
 
 /**
  * This class tests all subclasses of the abstract class Character.  Therfore, it also tests character itself, without directly 
@@ -22,26 +23,26 @@ public class CharacterTests extends TestCase {
 	 * tests the basic functions of Fish, such as decreasing its health and updating its x and y position
 	 */
 	public void testFish(){
-//		//Fish fish = new Fish(0,0);
-//		int h = fish.getHealth();
-//		fish.decreaseHealth(5);
-//		assertEquals(fish.getHealth(),h-5);
-//		double x =fish.getX();
-//		fish.move(10, 0);
-//		assertEquals(fish.getX(), x+10);
-//		double y = fish.getY();
-//		fish.move(0, 10);
-//		assertEquals(fish.getY(),y+10);
-//		
-//		//Fish fish2 = new Fish(0,0);
-//		Hook hook = new Hook(0,0,3);
-//		OverfishingGame game = new OverfishingGame();
-//		game.getEnemies().add(hook);
-//		game.getSchoolofFish().add(fish2);
-//		game.onTick();
-//		assertTrue(fish2.hasCollided());
-//		
-//		
+		Fish fish = new Fish(0,0, 0, 0);
+		int h = fish.getHealth();
+		fish.decreaseHealth(5);
+		assertEquals(fish.getHealth(),h-5);
+		double x =fish.getX();
+		fish.move(10, 0);
+		assertEquals(fish.getX(), x+10);
+		double y = fish.getY();
+		fish.move(0, 10);
+		assertEquals(fish.getY(),y+10);
+		
+	    Fish fish2 = new Fish(0,0,0,0);
+		Hook hook = new Hook(0,0,3);
+		OverfishingGame game = new OverfishingGame();
+		game.getEnemies().add(hook);
+		game.getSchoolofFish().add(fish2);
+		game.onTick();
+		assertTrue(fish2.hasCollided());
+		
+		
 		
 		
 		
@@ -52,26 +53,27 @@ public class CharacterTests extends TestCase {
 	 * tests the basic functions of Fish, such as decreasing its health and updating its x and y position
 	 */
 	public void testCrab(){
-		//Crab crab = new Crab(0,0,0);
-		//int h = crab.getHealth();
-//		crab.decreaseHealth(5);
-//		assertEquals(crab.getHealth(),h-5);
-//		double x =crab.getX();
-//		crab.move(10, 0);
-//		assertEquals(crab.getX(),10);
-//		double y = crab.getY();
-//		crab.move(0, 10);
-//		assertEquals(crab.getY(),y+10);
-//		
+		Crab crab = new Crab(0,0,0, null);
+		int h = crab.getHealth();
+		crab.decreaseHealth(5);
+		assertEquals(crab.getHealth(),h-5);
+		double x =crab.getX();
+		crab.move(10, 0);
+		assertEquals(crab.getX(),10);
+		double y = crab.getY();
+		crab.move(0, 10);
+		assertEquals(crab.getY(),y+10);
+		
 		Trash t = new Trash(0,0,Trash.BANANA);
 		double x2 = t.getX();
 		double y2 = t.getY();
-		//crab.throwTrash(t);
+		crab.holdTrash(t);
+		crab.throwAttachedTrash();
 		assertFalse(x2==t.getX() && y2 == t.getY());
 		
 		TheHuman human = new TheHuman(0, 0);
 		boolean b = human.hasFish();
-		//crab.pinchHumanToGetFish(human);
+		crab.pinchHumanToGetFish(human);
 		assertFalse(human.hasFish());
 		
 		
@@ -82,16 +84,14 @@ public class CharacterTests extends TestCase {
 	 * tests the basic functions of the Bubble class
 	 */
 	public void testBubble(){
-//		Bubble b = new Bubble(0,0);
-//		//Pollutant p = new Pollutant(0,0,0);
-//		//b.shootFromFish(p);
-//		double y = b.getY();
-//		assertEquals(b.getY(),y+2);
-//		double x = b.getX();
-//		assertEquals(b.getX(),x-1);
-//		b.pop();
-//		assertEquals(b.getX(),0);
-//		assertEquals(b.getY(),0);
+		Bubble b = new Bubble();
+		Pollutant p = new Pollutant(new Vector(0, 0),new Vector(1, 1),.1,Pollutant.FERTILIZER);
+		double y = b.getY();
+		assertEquals(b.getY(),y+2);
+		double x = b.getX();
+		assertEquals(b.getX(),x-1);
+		assertEquals(b.getX(),0);
+		assertEquals(b.getY(),0);
 		
 		
 		
