ad2f9a791f5a23ab83d6c36c5c96b244b133026f
Abraham McIlvaine <abemac@udel.edu>
2015-12-03 15:49:53 -0500

    New stuff

--- src/animation/OverfishingAnimation.java
+++ src/animation/OverfishingAnimation.java
@@ -42,13 +42,18 @@ public class OverfishingAnimation extends Animation{
 			setFadeOutTime(1.0);
 		}});
 		slides.add(new Slide("/Black.png", 1));
+		slides.add(new Slide("/Game1AnimationText1.png", 4));
+		slides.add(new Slide("/Game1AnimationText2.png", 6));
+		slides.add(new Slide("/Game1AnimationText3.png", 4));
+		slides.add(new Slide("/Black.png", 1));
 		slides.add(new Slide("/Game1AnimationSlide1.png", 4));
 		slides.add(new Slide("/Game1AnimationSlide2.png", 4));
 		slides.add(new Slide("/Game1AnimationSlide3.png", 4));
 		slides.add(new Slide("/Game1AnimationSlide4.png", 4){{
 		}});
-		slides.add(new Slide("/Game1AnimationSlide5.png", 4){{
+		slides.add(new Slide("/Game1AnimationSlide5.png", 6){{
 		}});
+		slides.add(new Slide("/Black.png", 1));
 		slides.add(new Slide("/Game1AnimationInstructionSlide.png",4){{
 			setFinalSlide(true);
 		}});
