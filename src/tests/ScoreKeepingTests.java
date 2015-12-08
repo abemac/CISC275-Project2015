d749b46418b01d1a743217c0767efb9eb5ed96e5
Arjun <patelaj94@yahoo.com>
2015-12-08 14:40:33 -0500

    New stuff

--- src/tests/ScoreKeepingTests.java
+++ src/tests/ScoreKeepingTests.java
@@ -1,8 +1,17 @@
 package tests;
 
+import scorekeeping.CrabSaveScore;
 import junit.framework.TestCase;
 
 public class ScoreKeepingTests extends TestCase{
-
+	public void testCrabSaveScore(){
+		CrabSaveScore crabSaveScore = new CrabSaveScore(0, 36.6);
+		assertEquals(crabSaveScore.getTrashNum(), 0);
+		assertEquals(crabSaveScore.getCalculatedScore(),100);
+		CrabSaveScore crabSaveScore1 = new CrabSaveScore(1, 24.4);
+		assertEquals(crabSaveScore1.getTrashNum(), 1);
+		assertEquals(crabSaveScore1.getTime(), 24.4);
+		assertEquals(crabSaveScore1.getCalculatedScore(),79);
+	}
 	
 }
